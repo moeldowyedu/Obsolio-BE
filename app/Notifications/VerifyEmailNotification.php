86d342496bf575fa0708767c46074bd86265b469
Mohammed <mohamed.eldawi@edu.svu.edu.eg>
2025-12-18 22:25:52 +0200

    push changes

--- app/Notifications/VerifyEmailNotification.php
+++ app/Notifications/VerifyEmailNotification.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Notifications;
 
 use Illuminate\Auth\Notifications\VerifyEmail;
@@ -16,7 +15,7 @@ class VerifyEmailNotification extends VerifyEmail
     {
         return URL::temporarySignedRoute(
             'verification.verify',
-            Carbon::now()->addHours(24), // Expires in 24 hours
+            Carbon::now()->addHours(24),
             [
                 'id' => $notifiable->getKey(),
                 'hash' => sha1($notifiable->getEmailForVerification()),
@@ -27,20 +26,9 @@ class VerifyEmailNotification extends VerifyEmail
     /**
      * Build the mail representation of the notification.
      */
-    /**
-     * Build the mail representation of the notification.
-     */
     public function toMail($notifiable)
     {
-        // 1. Generate the URL (Server-side signed route)
-        $verifyUrl = $this->verificationUrl($notifiable);
-
-        // 2. Transform into Frontend URL
-        $frontendUrl = \Illuminate\Support\Facades\Config::get('app.frontend_url', 'https://obsolio.com');
-        $query = parse_url($verifyUrl, PHP_URL_QUERY);
-
-        // Final Link: https://obsolio.com/verify-email/{id}/{hash}?signature=...
-        $actionUrl = "{$frontendUrl}/verify-email/{$notifiable->getKey()}/" . sha1($notifiable->getEmailForVerification()) . "?{$query}";
+        $actionUrl = $this->verificationUrl($notifiable);
 
         return (new MailMessage)
             ->subject('Activate Your OBSOLIO Workspace')
